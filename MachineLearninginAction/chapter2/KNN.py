fa4d4e290294300bc9e7120266ada9d01c4f19c0
OrangeS7 <fengchi-bing@foxmail.com>
2018-02-15 12:41:22 +0800

    update

---
+++ MachineLearninginAction/chapter2/KNN.py
@@ -0,0 +1,62 @@
+from numpy import *
+import operator
+
+def createDataSet():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+'''
+group, labels = createDataSet()
+result = classify0([0, 0], group, labels, 3)
+print(result)
+'''
+def file2matrix(filename):
+    with open(filename) as f:
+        arrayOLines = f.readlines()
+        numberOfLines = len(arrayOLines)
+        returnMat = zeros((numberOfLines, 3))
+        classLabelVector = []
+        index = 0
+        for line in arrayOLines:
+            line = line.strip()
+            listFromLine = line.split('\t')
+            returnMat[index, :] = listFromLine[0:3]
+            classLabelVector.append(int(listFromLine[-1]))
+            index += 1
+        return returnMat, classLabelVector
+
+import os
+filename = os.path.join(os.path.dirname('__file__'), 'datingTestSet2.txt')
+datingDataMat, datingLabels = file2matrix(filename)
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0*array(datingLabels), 15.0*array(datingLabels))
+plt.show()
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet / tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+
